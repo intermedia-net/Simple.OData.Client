c9e165f5fbe2c28906d32095c21a3477ece016f9
Dmitry Chistyakov <dchistyakov@intermedia.com>
2022-09-28 19:55:11 -0700

    Fix for filtering of entities on nested collections with Any() or All()

--- src/Simple.OData.Client.Core/Expressions/ODataExpression.Format.cs
+++ src/Simple.OData.Client.Core/Expressions/ODataExpression.Format.cs
@@ -195,16 +195,17 @@ public partial class ODataExpression
 	{
 		var navigationPath = FormatCallerReference();
 		EntityCollection entityCollection;
-		if (!context.Session.Metadata.HasNavigationProperty(context.EntityCollection.Name, navigationPath))
-		{
-			//simple collection property
-			entityCollection = context.EntityCollection;
-		}
-		else
+		try
 		{
 			entityCollection = context.Session.Metadata.NavigateToCollection(context.EntityCollection, navigationPath);
 		}
+		catch (UnresolvableObjectException)
+		{
+			//assume the simple collection property
+			entityCollection = null;
+		}
 
+		var formattedNavigationPath = context.Session.Adapter.GetCommandFormatter().FormatNavigationPath(context.EntityCollection, navigationPath);
 		string formattedArguments;
 		if (!Function.Arguments.Any() && string.Equals(Function.FunctionName, ODataLiteral.Any, StringComparison.OrdinalIgnoreCase))
 		{
@@ -217,7 +218,6 @@ public partial class ODataExpression
 			formattedArguments = $"{targetQualifier}:{FormatExpression(Function.Arguments.First(), expressionContext)}";
 		}
 
-		var formattedNavigationPath = context.Session.Adapter.GetCommandFormatter().FormatNavigationPath(context.EntityCollection, navigationPath);
 		return FormatScope($"{formattedNavigationPath}/{Function.FunctionName.ToLowerInvariant()}({formattedArguments})", context);
 	}
 
